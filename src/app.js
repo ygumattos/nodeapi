4b9c943d250600d113c87647b445b047c3374f3a
Ygor Mattos <ygormattos.b@gmail.com>
2019-10-25 20:06:57 -0300

    :heavy_plus_sign: Configurando Sucrase + Nodemon

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+
+class App{
+  constructor() {
+    this.server();
+
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+
+  }
+
+  routes() {
+    
+  }
+}
